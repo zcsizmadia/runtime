94719af602659881800b685131dc45eb553df78b
Zoltan Csizmadia <CsizmadiaZ@valassis.com>
2020-11-12 20:40:32 -0600

    : Add JsonPropertyOrderByNameAttribute

---
+++ src/libraries/System.Text.Json/src/System/Text/Json/Serialization/Attributes/JsonPropertyOrderByNameAttribute.cs
@@ -0,0 +1,36 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System.Collections.Generic;
+
+namespace System.Text.Json.Serialization
+{
+    /// <summary>
+    /// Specifies the property order in JSON when serializing.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct, AllowMultiple = false)]
+    public sealed class JsonPropertyOrderByNameAttribute : JsonAttribute
+    {
+        /// <summary>
+        /// Initializes a new instance of <see cref="JsonPropertyOrderByNameAttribute"/> with the specified property order.
+        /// </summary>
+        /// <param name="stringComparison">Property name comparison type.</param>
+        public JsonPropertyOrderByNameAttribute(StringComparison stringComparison)
+        {
+            StringComparison = stringComparison;
+        }
+
+        /// <summary>
+        /// Initializes a new instance of <see cref="JsonPropertyOrderByNameAttribute"/> with the specified property order.
+        /// </summary>
+        public JsonPropertyOrderByNameAttribute()
+            : this(StringComparison.Ordinal)
+        {
+        }
+
+        /// <summary>
+        /// Property name string comparison type.
+        /// </summary>
+        public StringComparison StringComparison { get; }
+    }
+}
